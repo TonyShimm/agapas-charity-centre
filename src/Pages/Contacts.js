37bb7699999272bc984e40d6c4d4e58b991784af
TonyShimm <101990825+TonyShimm@users.noreply.github.com>
2024-04-29 13:10:14 -0400

    update

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -9,20 +9,20 @@ export default class Contacts extends Component {
         <Form>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email adress</Form.Label>
-            <Form.Control type="email" placeholder="Enter email"></Form.Control>
+            <Form.Control type="email" placeholder="Enter email" />
             <Form.Text>We`ll never share your mail with anyone else</Form.Text>
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Example textarea</Form.Label>
-            <Form.Controll as="textarea" rows="3"></Form.Controll>
+            <Form.Controll as="textarea" rows="3" />
           </Form.Group>
 
           <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out"></Form.Check>
+            <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Submit{" "}
+            Submit
           </Button>
         </Form>
       </Container>
