497ab4522e34c81b7255171fe6f88aff0908cafd
TonyShimm <101990825+TonyShimm@users.noreply.github.com>
2024-04-29 12:27:57 -0400

    fix mistakes

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Media, Container, Col, Row, Card, ListGroup } from "react-bootstrap";
+import { Container, Col, Row, Card, ListGroup } from "react-bootstrap";
 
 export default class Blog extends Component {
   render() {
@@ -7,52 +7,52 @@ export default class Blog extends Component {
       <Container>
         <Row>
           <Col md="9">
-            <Media className="m-5">
+            <div className="media m-5">
               <img
                 width={150}
                 height={150}
                 className="mr-3"
                 scr="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
               />
-              <Media.Body>
+              <div className="media-body">
                 <h5>Blog post</h5>
                 <p>Lorem text 30</p>
-              </Media.Body>
-            </Media>
-            <Media className="m-5">
+              </div>
+            </div>
+            <div className="media m-5">
               <img
                 width={150}
                 height={150}
                 className="mr-3"
                 scr="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
               />
-              <Media.Body>
+              <div className="media-body">
                 <h5>Blog post</h5>
                 <p>Lorem text 30</p>
-              </Media.Body>
-            </Media>
-            <Media className="m-5">
+              </div>
+            </div>
+            <div className="media m-5">
               <img
                 width={150}
                 height={150}
                 className="mr-3"
                 scr="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
               />
-              <Media.Body>
+              <div className="media-body">
                 <h5>Blog post</h5>
                 <p>Lorem text 30</p>
-              </Media.Body>
-            </Media>
+              </div>
+            </div>
           </Col>
           <Col md="3">
             <h5 className="text-center mt-5 ">Categories</h5>
             <Card>
-              <List.Group variant="flush">
-                <List.Group.Item>Html/CSS</List.Group.Item>
-                <List.Group.Item>JavaScript</List.Group.Item>
-                <List.Group.Item>Phyton</List.Group.Item>
-                <List.Group.Item>C++</List.Group.Item>
-              </List.Group>
+              <ListGroup variant="flush">
+                <ListGroup.Item>Html/CSS</ListGroup.Item>
+                <ListGroup.Item>JavaScript</ListGroup.Item>
+                <ListGroup.Item>Phyton</ListGroup.Item>
+                <ListGroup.Item>C++</ListGroup.Item>
+              </ListGroup>
             </Card>
 
             <Card className="mt-3">
